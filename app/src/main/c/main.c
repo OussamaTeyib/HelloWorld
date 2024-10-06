6df69f3bc067326c6f48f2ff485627fc635c7802
Oussama Teyib <oussama.teyib@gmail.com>
2024-10-06 17:09:03 +0000

    Print "Hello, world!" on the screen

---
+++ app/src/main/c/main.c
@@ -0,0 +1,14 @@
+#include <raylib.h>
+
+int main() {
+    InitWindow(800, 600, "My App");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
